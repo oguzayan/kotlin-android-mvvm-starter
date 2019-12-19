134d43727937c2f6487aa75ba69d648c6c7e606c
oguzayan <ayan.oguz@gmail.com>
2019-12-19 17:09:30 +0300

    Update MainActivity.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/ui/main/MainActivity.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/ui/main/MainActivity.kt
@@ -4,10 +4,7 @@ import {{ cookiecutter.package_name }}.R
 import {{ cookiecutter.package_name }}.core.BaseActivity
 import {{ cookiecutter.package_name }}.databinding.ActivityMainBinding
 
-class MainActivity : BaseActivity<MainActivityViewModel, ActivityMainBinding>(MainActivityViewModel::class.java) {
-    override fun initViewModel(viewModel: MainActivityViewModel) {
-        binding.viewModel = viewModel
-    }
-
-    override fun getLayoutRes() = R.layout.activity_main
-}
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(
+    R.layout.activity_main,
+    MainViewModel::class.java
+)
