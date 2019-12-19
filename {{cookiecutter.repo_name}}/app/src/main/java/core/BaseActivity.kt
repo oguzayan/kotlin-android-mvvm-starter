52678b6a82ab34d25605efa2f1f54b291b17dbdb
oguzayan <ayan.oguz@gmail.com>
2019-12-19 16:53:09 +0300

    Update BaseActivity.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/core/BaseActivity.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/core/BaseActivity.kt
@@ -1,44 +1,36 @@
 package {{ cookiecutter.package_name }}.core
 
-import androidx.lifecycle.ViewModelProviders
-import androidx.databinding.DataBindingUtil
-import androidx.databinding.ViewDataBinding
 import android.os.Bundle
 import androidx.annotation.LayoutRes
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.databinding.ViewDataBinding
+import androidx.lifecycle.ViewModelProvider
+import androidx.lifecycle.ViewModelProviders
+import dagger.android.AndroidInjection
+import {{ cookiecutter.package_name }}.BR
+import javax.inject.Inject
 
-abstract class BaseActivity<VM : BaseViewModel, DB : ViewDataBinding>(private val mViewModelClass: Class<VM>) : AppCompatActivity() {
+abstract class BaseActivity<B : ViewDataBinding, V : BaseViewModel>(
+    @LayoutRes val layout: Int, viewModelClass: Class<V>
+) : AppCompatActivity() {
 
-    @LayoutRes
-    abstract fun getLayoutRes(): Int
+    @Inject
+    internal lateinit var viewModelProviderFactory: ViewModelProvider.Factory
 
-    val binding by lazy {
-        DataBindingUtil.setContentView(this, getLayoutRes()) as DB
+    val binding: B by lazy {
+        DataBindingUtil.setContentView(this, layout) as B
     }
 
-    val viewModel by lazy {
-        ViewModelProviders.of(this).get(mViewModelClass)
+    private val viewModel by lazy {
+        ViewModelProviders.of(this, viewModelProviderFactory).get(viewModelClass)
     }
-
-    /**
-     * If you want to inject Dependency Injection
-     * on your activity, you can override this.
-     */
-    open fun onInject() {}
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        initViewModel(viewModel)
+        AndroidInjection.inject(this)
 
         super.onCreate(savedInstanceState)
-
-        onInject()
+        binding.lifecycleOwner = this
+        binding.setVariable(BR.viewModel, viewModel)
     }
-
-    /**
-     *
-     *  You need override this method.
-     *  And you need to set viewModel to binding: binding.viewModel = viewModel
-     *
-     */
-    abstract fun initViewModel(viewModel: VM)
 }
