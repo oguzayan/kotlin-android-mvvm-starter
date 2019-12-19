1bedec35845da72407dd1ebfc213e754e0c8ec2b
oguzayan <ayan.oguz@gmail.com>
2019-12-19 17:04:20 +0300

    Update App.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/App.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/App.kt
@@ -1,14 +1,38 @@
 package {{ cookiecutter.package_name }}
 
+import android.app.Activity
+import android.app.Application
+import dagger.android.AndroidInjector
+import dagger.android.DispatchingAndroidInjector
+import dagger.android.HasActivityInjector
 import {{ cookiecutter.package_name }}.di.component.DaggerApplicationComponent
-import {{ cookiecutter.package_name }}.di.module.ApplicationModule
+import timber.log.Timber
+import timber.log.Timber.DebugTree
+import javax.inject.Inject
 
-class App : android.app.Application() {
 
-    val component by lazy {
+class BatteryApplication : Application(), HasActivityInjector {
+
+    @Inject
+    lateinit var activityInjector: DispatchingAndroidInjector<Activity>
+
+    override fun activityInjector(): AndroidInjector<Activity> {
+        return activityInjector
+    }
+
+    override fun onCreate() {
+        super.onCreate()
+
         DaggerApplicationComponent.builder()
-                .applicationModule(ApplicationModule(this))
-                .build()
+            .applicationBind(this)
+            .build()
+            .inject(this)
+
+        if (BuildConfig.DEBUG) {
+            Timber.plant(DebugTree())
+        } else {
+            //TODO : Add crashlytics
+        }
     }
 }
 
