65b22aad516b637dd1db4cba31726bf56782fa7d
oguzayan <ayan.oguz@gmail.com>
2019-12-19 16:55:02 +0300

    Update BaseAdapter.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/core/BaseAdapter.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/core/BaseAdapter.kt
@@ -8,16 +8,16 @@ import {{ cookiecutter.package_name }}.R
 
 abstract class BaseAdapter<T>(callback: DiffUtil.ItemCallback<T>) : ListAdapter<T, BaseViewHolder<ViewDataBinding>>(callback) {
 
-    override fun onBindViewHolder(holder: BaseViewHolder<ViewDataBinding>, position: Int) {
-        (holder as BaseViewHolder<*>).binding.root.setTag(R.string.position, position)
+   override fun onBindViewHolder(holder: BaseViewHolder<ViewDataBinding>, position: Int) {
         bind(holder.binding, position)
     }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) = getViewHolder(parent, viewType)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder<ViewDataBinding> {
+        return getViewHolder(parent, viewType)
+    }
 
     open fun getViewHolder(parent: ViewGroup, viewType: Int) = BaseViewHolder(createBinding(parent, viewType))
 
     abstract fun createBinding(parent: ViewGroup, viewType: Int): ViewDataBinding
-
     protected abstract fun bind(binding: ViewDataBinding, position: Int)
 }
