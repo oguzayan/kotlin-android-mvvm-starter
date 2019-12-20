cc484ff1fc5b0ad7923e3e7d85216201feaf5b77
oguzayan <ayan.oguz@gmail.com>
2019-12-20 10:41:57 +0300

    Update ApplicationComponent.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/di/component/ApplicationComponent.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/di/component/ApplicationComponent.kt
@@ -12,20 +12,26 @@ import javax.inject.Singleton
 
 @Singleton
 {% if cookiecutter.retrofit == "y" %}
-@Component(modules = [ApplicationModule::class,
+@Component(
+  modules = [ApplicationModule::class,
                       AndroidInjectionModule::class,
                       NetModule::class,
                       DatabaseModule::class,
                       ActivityModule::class,
                       ViewModelModule::class]
+  )
 {% endif %}
+  
 {% if cookiecutter.retrofit == "n" %}
-@Component(modules = [ApplicationModule::class,
+@Component(
+  modules = [ApplicationModule::class,
                       AndroidInjectionModule::class,
                       DatabaseModule::class,
                       ActivityModule::class,
                       ViewModelModule::class]
+  )
 {% endif %}
+ 
 interface ApplicationComponent : AndroidInjector<App> {
 
     @Component.Builder
