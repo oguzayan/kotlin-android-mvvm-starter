ecfc78a34739982446db644d608dc2465e39e593
oguzayan <ayan.oguz@gmail.com>
2020-01-06 09:48:56 +0300

    Update MainActivityViewModel.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/ui/main/MainActivityViewModel.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/ui/main/MainActivityViewModel.kt
@@ -1,6 +1,7 @@
 package {{ cookiecutter.package_name }}.ui.main
 
 import android.content.Context
+import {{ cookiecutter.package_name }}.BuildConfig
 import {{ cookiecutter.package_name }}.core.BaseViewModel
 import javax.inject.Inject
 
