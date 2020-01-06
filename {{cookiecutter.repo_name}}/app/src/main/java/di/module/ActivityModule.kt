13ac365fa403629962bb7303069c945ae941d7fe
oguzayan <ayan.oguz@gmail.com>
2020-01-06 09:42:14 +0300

    Update ActivityModule.kt

--- {{cookiecutter.repo_name}}/app/src/main/java/di/module/ActivityModule.kt
+++ {{cookiecutter.repo_name}}/app/src/main/java/di/module/ActivityModule.kt
@@ -4,9 +4,7 @@ import dagger.Module
 import dagger.android.ContributesAndroidInjector
 import {{ cookiecutter.package_name }}.di.scope.ActivityScope
 import {{ cookiecutter.package_name }}.ui.main.MainActivity
-import dagger.android.ContributesAndroidInjector
-import {{ cookiecutter.package_name }}.di.scope.ActivityScope
-import {{ cookiecutter.package_name }}.ui.main.MainActivity
+
 
 @Module
 abstract class ActivityModule {
